86f32389ee6845b5a4363cbdac0689cf7712d1c2
Gus <gusrichardson1@gmail.com>
2019-11-28 21:09:54 -0500

    tablet media query for album

---
+++ script.js
@@ -0,0 +1,7 @@
+// console.log("hello");
+
+// const albumLink = document.getElementById('album-link');
+
+// albumLink.addEventListener("click", function () {
+//     window.scrollBy(0, -800)
+// })
