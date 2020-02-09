d1a4878dd296f4f34980c500bdb88adab5ffdd08
Gus <gusrichardson1@gmail.com>
2020-02-09 14:03:25 -0500

    steve changes and fixed image popup

--- art.js
+++ art.js
@@ -9,8 +9,15 @@ $(document).ready(function () {
 
     $('.art-image').click(function () {
         let $imgSrc = $(this).attr('src');
-        let $imgPos = $(this).offset().top / 1.5;
-        $artImg.attr('src', $imgSrc).css({ 'margin-top': $imgPos });
+        let $documentHeight = $(document).height();
+        console.log("this is $documentHeight", $documentHeight);
+        let $windowHeight = $(window).height();
+        console.log("this is the window height", $windowHeight);
+        let $imgPos = $(this).offset().top
+        console.log("this is the $imgPos", $imgPos);
+        let pixels = $imgPos + 'px';
+        // let $imgPos = $(this).offset().top / 1.5;
+        $artImg.attr('src', $imgSrc).css({ 'margin-top': pixels });
         let $artHeight = $artImg.height();
         $artOverlay.css({ 'display': 'block' });
     })
