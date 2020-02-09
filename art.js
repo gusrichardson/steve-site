72b284814ef6f7e9e598492de229f6db0908bcc3
Gus <gusrichardson1@gmail.com>
2020-02-09 14:37:49 -0500

    fixed desktop image popup issue

--- art.js
+++ art.js
@@ -9,18 +9,18 @@ $(document).ready(function () {
 
     $('.art-image').click(function () {
         let $imgSrc = $(this).attr('src');
-        let $documentHeight = $(document).height();
-        console.log("this is $documentHeight", $documentHeight);
-        let $windowHeight = $(window).height();
-        // let $windowTop = $(window).position();
-        // console.log("this is the windowTop", $windowTop);
-        console.log("this is the window height", $windowHeight);
         let $imgPos = $(this).offset().top
-        console.log("this is the $imgPos", $imgPos);
-        let pixels = $imgPos + 'px';
-        // let $imgPos = $(this).offset().top / 1.5;
-        $artImg.attr('src', $imgSrc).css({ 'margin-top': pixels });
-        let $artHeight = $artImg.height();
+        if ($(window).width() > 800) {
+            console.log("greater than 800")
+            let position = Math.floor($imgPos/1.5) + "px";
+            console.log("this is position", position);
+            $artImg.attr('src', $imgSrc).css({ 'margin-top': position });
+            //let $imgPos = $(this).offset().top / 1.5;
+        } else {
+            let pixels = $imgPos + 'px';
+            $artImg.attr('src', $imgSrc).css({ 'margin-top': pixels });
+        }
+        // let $artHeight = $artImg.height();
         $artOverlay.css({ 'display': 'block' });
     })
     $artOverlay.click(function () {
